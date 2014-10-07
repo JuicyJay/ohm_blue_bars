9e3c69f1fe6f5051d9f03fb9e124bb2b63ea6e08
Knueppl <knueppl@NCT>
2014-10-07 18:59:27 +0200

    Package ohm_exploration: added a subriber for the current laser pose.

--- src/ohm_exploration/src/exploration-node.cpp
+++ src/ohm_exploration/src/exploration-node.cpp
@@ -1,8 +1,20 @@
 #include <ros/ros.h>
 #include <tf/tf.h>
+#include <tf/transform_listener.h>
 
 #include "Sensor.h"
 #include "SensorPublisher.h"
+
+tf::StampedTransform _transform;
+
+void callbackPose(const geometry_msgs::PoseStamped& pose)
+{
+    _transform.setOrigin(tf::Vector3(pose.pose.position.x, pose.pose.position.y, pose.pose.position.z));
+    _transform.setRotation(tf::Quaternion(pose.pose.orientation.x,
+                                          pose.pose.orientation.y,
+                                          pose.pose.orientation.z,
+                                          pose.pose.orientation.w));
+}
 
 int main(int argc, char** argv)
 {
@@ -11,14 +23,23 @@ int main(int argc, char** argv)
     Sensor sensor;
     SensorPublisher pubSensor(sensor, nh);
     ros::Rate rate(10);
+    ros::Subscriber subPose(nh.subscribe("slam_out_pose", 1, callbackPose));
+//    tf::TransformListener listener;
 
     while (ros::ok())
     {
-        tf::Transform transform;
 
-        transform.setOrigin(tf::Vector3(0.0f, 0.0f, 1.0f));
-
-        pubSensor.publish(transform);
+        pubSensor.publish(_transform);
+//        if (listener.waitForTransform("laser", "map", ros::Time::now(), ros::Duration(0.1f)))
+//        try
+//        {
+//            listener.lookupTransform("laser", "map", ros::Time(0), transform);
+//            pubSensor.publish(transform);
+//        }
+//        catch (tf::TransformException ex)
+//        {
+//            ROS_ERROR("%s", ex.what());
+//        }
 
         ros::spinOnce();
         rate.sleep();
