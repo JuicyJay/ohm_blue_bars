92c13355b8d3dec2a6d220ab8dc0e160ba3ae99b
knueppl <knueppl@base>
2015-03-29 21:30:46 +0200

    Exploration: just saved.

---
+++ src/ohm_exploration/src/mission-planner-node.cpp
@@ -0,0 +1,15 @@
+/***********************************************************************************************************
+ * mission-planner-node
+ *
+ *  Created on: 29.03.2015
+ *      Author: Christian Merkl
+ *      E-Mail: christian.merkl@th-nuernberg.de
+ *     Licence: BSD
+ *
+ ***********************************************************************************************************/
+#include <ros/ros.h>
+
+int main(int argc, char** argv)
+{
+    ros::init(argc, argv, "mission_planner");
+}
